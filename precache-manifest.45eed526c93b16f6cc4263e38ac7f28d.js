ab5cbcdc2796bda48e0658e5dddf9d0187ca9e88
pankaj sharma <pankajsharma123@gmail.com>
2020-10-31 20:41:04 +0530

    Updates

---
+++ precache-manifest.45eed526c93b16f6cc4263e38ac7f28d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf834f019c570ee6257b7cc1994ef45",
+    "url": "/react-pro1/index.html"
+  },
+  {
+    "revision": "a81627d92a7655fafc43",
+    "url": "/react-pro1/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "2455fcd12a4f912a625a",
+    "url": "/react-pro1/static/css/main.2b61c674.chunk.css"
+  },
+  {
+    "revision": "a81627d92a7655fafc43",
+    "url": "/react-pro1/static/js/2.f9da9f64.chunk.js"
+  },
+  {
+    "revision": "e53cf286e00aab3498ae2b2d3185567a",
+    "url": "/react-pro1/static/js/2.f9da9f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2455fcd12a4f912a625a",
+    "url": "/react-pro1/static/js/main.8d1199e5.chunk.js"
+  },
+  {
+    "revision": "f3804cc7338638e65cf5",
+    "url": "/react-pro1/static/js/runtime-main.32e5a764.js"
+  },
+  {
+    "revision": "00417309a3ca94798ddb4df81f45abc7",
+    "url": "/react-pro1/static/media/buss1.00417309.png"
+  },
+  {
+    "revision": "08485655ab6b199d8a8682514e77a088",
+    "url": "/react-pro1/static/media/viewscreen.08485655.svg"
+  }
+]);
